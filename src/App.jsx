c385161883c4b24a3a8c0c8a0826f2c6ead37d37
adawatia <122260173+adawatia@users.noreply.github.com>
2025-03-12 01:49:02 +0530

    Navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import { useState, useCallback } from "react";
 import { LoadingScreen } from "./components/LoadingScreen";
+import { Navbar } from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
 import "./index.css";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   // Use useCallback to prevent unnecessary re-renders
   const handleComplete = useCallback(() => {
@@ -20,7 +23,8 @@ function App() {
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
       >
-        {/* Content Goes Here */}
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       </div>
     </>
   );
